06c7b8f43b4b87a6a641127ce108ef0ee0bc6c47
DarkFighterLuke <luca-consoli@live.it>
2019-12-06 11:21:41 +0100

    - CR to CRLF

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -1,58 +1,56 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: luca-
- * Date: 22/09/2019
- * Time: 16:08
- */
-
-namespace App\Controller;
-
-
-use App\Entity\LoginModel;
-use App\Entity\User;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
-
-
-class LoginController extends AbstractController
-{
-    private $user;
-    private $model;
-
-    public function __construct(){
-        session_start();
-        $this->model=new LoginModel();
-    }
-
-    /**
-     * @Route("/login", methods="POST")
-     */
-    public function checkLogin(){
-        $username=isset($_POST['username'])?$_POST['username']:null;
-        $password=isset($_POST['password'])?$_POST['password']:null;
-
-        $result=$this->model->checkLogin($username, $password);
-
-        if($result){
-            return $this->render("login.html.twig",["user" => $_SESSION['user']]);
-        }
-        else{
-            return $this->render("login.html.twig",["error" => true]);
-        }
-    }
-
-    /**
-     * @Route("/logout", name="logout")
-     */
-    public function logout(){
-        /*$session=$request->getSession();
-        $session->clear();
-        $session->set("status",0);*/
-        $this->user=null;
-        $_SESSION["user"]=null;
-        session_destroy();
-        //return $this->render("home.html.twig",["user" => null]);
-        return $this->redirectToRoute("home");
-    }
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luca-
+ * Date: 22/09/2019
+ * Time: 16:08
+ */
+
+namespace App\Controller;
+
+use App\Entity\LoginModel;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LoginController extends AbstractController
+{
+    private $user;
+    private $model;
+
+    public function __construct()
+    {
+        session_start();
+        $this->model = new LoginModel();
+    }
+
+    /**
+     * @Route("/login", methods="POST")
+     */
+    public function checkLogin()
+    {
+        $username = isset($_POST['username']) ? $_POST['username'] : null;
+        $password = isset($_POST['password']) ? $_POST['password'] : null;
+        $result = $this->model->checkLogin($username, $password);
+        if ($result) {
+            return $this->render("login.html.twig", ["user" => $_SESSION['user']]);
+        } else {
+            return $this->render("login.html.twig", ["error" => true]);
+        }
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout()
+    {
+        /*$session=$request->getSession();
+        $session->clear();
+        $session->set("status",0);*/
+        $this->user = null;
+        $_SESSION["user"] = null;
+        session_destroy();
+        //return $this->render("home.html.twig",["user" => null]);
+        return $this->redirectToRoute("home");
+    }
 }

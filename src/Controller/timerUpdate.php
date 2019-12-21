64ab823fde659c2f3dd303ad4348448edbe9a24a
DarkFighterLuke <luca-consoli@live.it>
2019-12-21 10:25:51 +0100

    modified:   timerUpdate.php

--- src/Controller/timerUpdate.php
+++ src/Controller/timerUpdate.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-use Psr\Log\LoggerInterface;
-
 class timerUpdate
 {
     private $intervalDays = 7;
@@ -17,7 +15,7 @@ class timerUpdate
     public function timer()
     {
         $now = new \DateTime();
-        if ((int)$now->diff($this->lastUpdate, true)->format('%d') % $this->intervalDays == 0) {
+        if ($this->days_diff($now, $this->lastUpdate) % $this->intervalDays == 0) {
             $this->lastUpdate = $now;
             error_log("true", 0);
             return true;
@@ -25,5 +23,32 @@ class timerUpdate
             error_log("false", 0);
             return false;
         }
+    }
+    
+    function days_diff($d1, $d2) {
+        $x1 = $this->days($d1);
+        $x2 = $this->days($d2);
+       
+        if ($x1 && $x2) {
+            return abs($x1 - $x2);
+        }
+    }
+    
+    function days($x) {
+        if (get_class($x) != 'DateTime') {
+            return false;
+        }
+       
+        $y = $x->format('Y') - 1;
+        $days = $y * 365;
+        $z = (int)($y / 4);
+        $days += $z;
+        $z = (int)($y / 100);
+        $days -= $z;
+        $z = (int)($y / 400);
+        $days += $z;
+        $days += $x->format('z');
+    
+        return $days;
     }
 }
